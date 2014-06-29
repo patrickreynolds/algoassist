ba30c3f835d02a212b04e855e22d136e5031795e
Patrick Reynolds <patrickscottreynolds@gmail.com>
2014-06-28 19:07:28 -0700

    Change partial conersion to new module structure.

--- lib/algoassist.rb
+++ lib/algoassist.rb
@@ -1,26 +1,7 @@
 # Algorithms Classes
-class Insertion_Sort
-	def self.info
-		puts "Name: Insertion Sort"
-		puts "O Notation:"
-		puts "Best: O(n^2)"
-		puts "Worst: O(n)"
+module Algoassist
+	def info
+		puts "Algoassist is a gem used to teach \nstandard searching, sorting, and graph algorithms."
 	end
-
-	def self.sort(numbers)
-  	1.upto(numbers.count - 1) do |index|
-    	self.insert(numbers, index, numbers[index])
-  	end
-  	numbers
-	end
-
-	private
-	def self.insert(numbers, index, value)
-	  previous_index = index - 1
-	  while (previous_index >= 0) && (numbers[previous_index] > value)
-	    numbers[previous_index + 1] = numbers[previous_index]
-	    previous_index -= 1
-	  end
-	  numbers[previous_index + 1] = value
-	end
+	require 'algoassist/insertion_sort.rb'
 end
